3f8bce46f06a4f772c7ea0fa5c79745171ac4615
shaozh <hope128384348@gmail.com>
2022-11-08 13:14:09 +0800

    1.0 version

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import todolist from './components/toDoList.vue'
+import cat from './components/cat.vue'
+</script>
+
+<template>
+  <h1>To Do List</h1>
+  <todolist/>
+  <cat></cat>
+</template>
+
+<style scoped>
+h1{
+  text-shadow: 2px 0 #BDFCC9;
+}
+</style>
---
+++ src/components/toDoList.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+let id = 0
+const newToDo = ref('')
+const toDos = ref([])
+
+function addToDo() {
+  toDos.value.push({id: id++, text:newToDo.value})
+   newToDo.value = ''
+}
+function delToDo(toDo) {
+  toDos.value = toDos.value.filter((t) => t.id !== toDo.id)
+}
+</script>
+
+
+
+<template>
+  <div id="insert">
+    <input type="text" v-model="newToDo" placeholder="請輸入">
+    <button @click="addToDo" id="newli">add</button>
+  </div>
+ 
+ <ul>
+  <li v-for="toDo in toDos" :key="toDo.id">
+    {{toDo.text}}
+    <button @click="delToDo(toDo)">delete</button>
+  </li>
+ </ul>
+</template>
+
+
+<style scoped>
+#insert{
+  text-align: center;
+}
+button{
+  border-radius: 5px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 0.5em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+#newli:hover {
+  border-color: #646cff;
+}
+
+</style>
